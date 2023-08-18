8ecc2f83fb0c727fc1efc7630f89ad93eb6fd92a
syed ibrahim shikalgar <syedibrahim7755@gamail.com>
2023-08-18 21:00:34 +0530

    feat added usestate

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { database } from "../utils/Database";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(database);
+
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const result = database.filter(
+              (filter_res) => filter_res?.info?.avgRating > 4.2
+            );
+            setListOfRestaurants(result);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {database.map((item) => {
+        {listOfRestaurants.map((item) => {
           return <RestaurantCard key={item?.info?.id} resData={item} />;
         })}
       </div>
