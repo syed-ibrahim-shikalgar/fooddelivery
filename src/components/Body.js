a32d1be828545a1de367440d0ba8eccd39e95963
syed ibrahim shikalgar <syedibrahim7755@gamail.com>
2023-08-21 15:58:09 +0530

    Live api is added with shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { database } from "../utils/Database";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(database);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.982946609962298&lng=77.4884595599501&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants?.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
@@ -11,7 +30,7 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            const result = database.filter(
+            const result = listOfRestaurants.filter(
               (filter_res) => filter_res?.info?.avgRating > 4.2
             );
             setListOfRestaurants(result);
@@ -21,7 +40,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((item) => {
+        {listOfRestaurants?.map((item) => {
           return <RestaurantCard key={item?.info?.id} resData={item} />;
         })}
       </div>
