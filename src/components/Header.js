1f4c05b1152d3a638104ff7a8c19c70f02417fab
syed ibrahim shikalgar <syedibrahim7755@gamail.com>
2023-08-21 16:45:33 +0530

    feat added login and logout btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  // let btnName = "Login";
+
+  const [btnName, setbtnName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +16,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
