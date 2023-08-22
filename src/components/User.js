9bcd69dd466a80f6f0d44da13cb859bc6cb27df2
syed ibrahim shikalgar <syedibrahim7755@gamail.com>
2023-08-23 00:47:36 +0530

    feat added usestate in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const User = ({ name }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
+      <h2>Count = {count}</h2>
+      <h2>Count2 = {count2}</h2>
       <h2>Name: {name}</h2>
       <h3>Location: Bengaluru</h3>
       <h4>contact: syedibrahim7755@gmail.com</h4>
